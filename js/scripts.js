2274a7bd07526c18ce30d65dc896722ff6dcc974
Slick Rick <slickrick@MikieshadowsMBP.hsd1.il.comcast.net>
2017-12-13 11:43:28 -0600

    add js and css

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('img').hide();
+    $('img').fadeIn(4000);
+    $('#toolbar').hide();
+    $('#toolbar').slideDown(5000);
+    $('p').hide();
+    $('p').fadeIn(2000);
+    $('#header').hide();
+    $('#header').fadeIn(10000);
+});
